3a04808976f5d7a0fb38e5b4d61c2f56e1266e03
Priyanka-Singh-47 <priyanka.singh@47billion.com>
2021-06-07 19:00:35 +0530

    practicing git and node

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,18 +4,17 @@ const userModel = require("../models/user");
 
 const signUp = async (req, res) => {
   try {
-    console.log("I am inside signup data");
+    // console.log("I am inside signup data");
 
     const { name, email, gender, city, password } = req.body;
-    console.log("this is req.body", req.body);
+    // console.log("this is req.body", req.body);
 
     const salt = await bcrypt.genSalt(10);
     let hashedPassword = bcrypt.hashSync(password, salt);
 
     const userdata = await userModel.findOne({ email: email });
-    console.log(userdata, "userdata");
-    // let profileImage = req.files;
-    // console.log(profileImage);
+    // console.log(userdata, "userdata");
+
     if (!userdata) {
       var mydata = new userModel({
         name,
@@ -32,7 +31,7 @@ const signUp = async (req, res) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(mydata);
+        // console.log(mydata);
         res.status(200).json("User is registered successfully");
       }
     });
@@ -45,9 +44,9 @@ const signUp = async (req, res) => {
 const userLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(req.body);
+    // console.log(req.body);
     const userdata = await userModel.findOne({ email: email });
-    console.log(userdata.password);
+    // console.log(userdata.password);
     const match = await bcrypt.compare(password, userdata.password);
     if (match) {
       res.status(200).json("you are signed in successfully");
@@ -63,7 +62,7 @@ const userLogin = async (req, res) => {
 const viewProfile = async (req, res) => {
   try {
     const id = req.params.id;
-    console.log(id);
+    // console.log(id);
 
     const userdata = await userModel.findOne({ _id: id });
     // console.log(userdata);
@@ -74,4 +73,48 @@ const viewProfile = async (req, res) => {
   }
 };
 
-module.exports = { signUp, userLogin, viewProfile };
+const updateDetails = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userdata = await userModel.findOne({ _id: id });
+
+    //  console.log(userdata);
+    var myNewData = {
+      name: req.body.name || userdata.name,
+      gender: req.body.gender || userdata.email,
+      city: req.body.city || userdata.city,
+    };
+    await userModel.updateOne({ _id: id }, myNewData, function (err) {
+      if (err) res.status(500).json("unable to update user data");
+      res.status(200).json("data is updated successfully");
+    });
+  } catch (error) {
+    res.status(400).json("unexpected error occured");
+    console.log(error);
+  }
+};
+
+const deleteProfile = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    await userModel.findOneAndDelete({ _id: id }, function (err) {
+      if (err) {
+        res.status(500).json("unable to delete user");
+      } else {
+        res.status(200).json("your profile is deleted successfully");
+      }
+    });
+  } catch (error) {
+    res.status(400).json("unexpected error occured");
+    console.log(error);
+  }
+};
+
+module.exports = {
+  signUp,
+  userLogin,
+  viewProfile,
+  updateDetails,
+  deleteProfile,
+};
--- src/router/v1/user.js
+++ src/router/v1/user.js
@@ -6,5 +6,7 @@ const { userController } = require("../../controllers/index");
 router.post("/signUp", userController.signUp);
 router.post("/userLogin", userController.userLogin);
 router.get("/profile/:id", userController.viewProfile);
+router.patch("/updateUser/:id",userController.updateDetails);
+router.delete("/deleteUser/:id",userController.deleteProfile);
 
 module.exports = router;
